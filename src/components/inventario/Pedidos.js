e5ce5d64d5911fd22f758c12f67566b0097a0f70
Oswaldinho <oswalfut_96@hotmail.com>
2017-12-19 19:16:14 -0600

    vamoooos

--- src/components/inventario/Pedidos.js
+++ src/components/inventario/Pedidos.js
@@ -3,17 +3,23 @@
  */
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {Table, TableHeaderColumn, TableHeader, TableBody, TableRow, TableRowColumn} from 'material-ui';
+import {Table, TableHeaderColumn, TableHeader, TableBody, TableRow, TableRowColumn, FloatingActionButton, Dialog, MenuItem, SelectField, TextField, RaisedButton} from 'material-ui';
 import {bindActionCreators} from 'redux';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 
 
 class Pedidos extends Component {
-    state = {};
+    state = {
+        openForm:false
+    };
 
+    handleOpenForm = () => {
+        this.setState({openForm:!this.state.openForm});
+    };
     render() {
         return (
             <div style={{padding:'3% 6%'}}>
-                <h2>Últimos Pedidos</h2>
+                <h2>Pedidos Surtidos</h2>
                 <Table>
                     <TableHeader>
                         <TableRow>
@@ -52,6 +58,60 @@ class Pedidos extends Component {
 
                     </TableBody>
                 </Table>
+                <FloatingActionButton
+                    onClick={this.handleOpenForm}
+                    backgroundColor={"grey"}
+                    style={{position:"fixed", right:25, bottom:25}}>
+                    <ContentAdd />
+                </FloatingActionButton>
+                <Dialog
+                    contentStyle={{width:'30%'}}
+                    open={this.state.openForm}
+                    onRequestClose={this.handleOpenForm}
+                >
+                    <form action="">
+                        <SelectField
+                            fullWidth={true}
+                            floatingLabelText="Cliente"
+                            value={1}
+
+                        >
+                            <MenuItem value={1} primaryText="Super Chelas" />
+                            <MenuItem value={2} primaryText="Expendio Beeer" />
+                            <MenuItem value={3} primaryText="Miscelanea Don Gorro" />
+                            <MenuItem value={4} primaryText="Bar el Oso" />
+                            <MenuItem value={5} primaryText="La barata" />
+                        </SelectField>
+                        <br/>
+
+                        <SelectField
+                            fullWidth={true}
+                            floatingLabelText="Producto"
+                            value={1}
+
+                        >
+                            <MenuItem value={1} primaryText="Brown Ale" />
+                            <MenuItem value={2} primaryText="IPA" />
+                            <MenuItem value={3} primaryText="Golden Ale" />
+                            <MenuItem value={4} primaryText="Agave Lager" />
+
+                        </SelectField>
+                        <br/>
+                        <TextField
+                            fullWidth={true}
+                            floatingLabelFixed={true}
+                            floatingLabelText="Cantidad"
+                            hintText="Cantidad"/>
+                        <br/>
+                        <TextField
+                            fullWidth={true}
+                            floatingLabelFixed={true}
+                            floatingLabelText="Presentacion"
+                            hintText="Presentacion"/>
+                        <br/>
+                        <RaisedButton label={'Enviar'} fullWidth={true}/>
+                    </form>
+                </Dialog>
             </div>
         );
     }
--- src/components/inventario/SolicitarProducto.js
+++ src/components/inventario/SolicitarProducto.js
@@ -1,72 +1,134 @@
-import React from 'react';
-import {TextField, Paper, SelectField, MenuItem, GridList, GridTile, RaisedButton} from 'material-ui';
+/**
+ * Created by BlisS on 22/03/17.
+ */
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Table, TableHeaderColumn, TableHeader, TableBody, TableRow, TableRowColumn, FloatingActionButton, Dialog, MenuItem, SelectField, TextField, RaisedButton} from 'material-ui';
+import {bindActionCreators} from 'redux';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 
-const SolicitarProducto = ({props}) => {
-    return (
-        <div style={{padding:'3% 6%'}}>
-           <GridList cols={3} cellHeight={'auto'}>
-               <GridTile cols={2} style={{padding:'3%'}}>
-                   <Paper zDepth={2} >
 
-                       <SelectField
-                           floatingLabelText="Cliente"
-                           value={1}
+class SolicitarProducto extends Component {
+    state = {
+        openForm:false
+    };
 
-                       >
-                           <MenuItem value={1} primaryText="Super Chelas" />
-                           <MenuItem value={2} primaryText="Expendio Beeer" />
-                           <MenuItem value={3} primaryText="Miscelanea Don Gorro" />
-                           <MenuItem value={4} primaryText="Bar el Oso" />
-                           <MenuItem value={5} primaryText="La barata" />
-                       </SelectField>
-                       <br/>
+    handleOpenForm = () => {
+        this.setState({openForm:!this.state.openForm});
+    };
+    render() {
+        return (
+            <div style={{padding:'3% 6%'}}>
+                <h2>Mis Ordenes</h2>
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHeaderColumn>ID</TableHeaderColumn>
+                            <TableHeaderColumn>Fecha</TableHeaderColumn>
+                            <TableHeaderColumn>Monto</TableHeaderColumn>
+                            <TableHeaderColumn>Status</TableHeaderColumn>
+                            <TableHeaderColumn>Recibido</TableHeaderColumn>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow>
+                            <TableRowColumn>1234wert</TableRowColumn>
+                            <TableRowColumn>12-ago-2017</TableRowColumn>
+                            <TableRowColumn>$20000</TableRowColumn>
+                            <TableRowColumn>Pagado</TableRowColumn>
+                            <TableRowColumn>Si</TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>h3h3hh2h2h</TableRowColumn>
+                            <TableRowColumn>15-sep-2017</TableRowColumn>
+                            <TableRowColumn>$10000</TableRowColumn>
+                            <TableRowColumn>Pagado</TableRowColumn>
+                            <TableRowColumn>Si</TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>9ds9ew</TableRowColumn>
+                            <TableRowColumn>12-oct-2017</TableRowColumn>
+                            <TableRowColumn>$15000</TableRowColumn>
+                            <TableRowColumn>Pagado</TableRowColumn>
+                            <TableRowColumn>Si</TableRowColumn>
+                        </TableRow>
+                        <TableRow>
+                            <TableRowColumn>4h3h4h8</TableRowColumn>
+                            <TableRowColumn>20-nov-2017</TableRowColumn>
+                            <TableRowColumn>$30000</TableRowColumn>
+                            <TableRowColumn>Pendiente</TableRowColumn>
+                            <TableRowColumn>No</TableRowColumn>
+                        </TableRow>
 
-                       <SelectField
-                           floatingLabelText="Producto"
-                           value={1}
 
-                       >
-                           <MenuItem value={1} primaryText="Brown Ale" />
-                           <MenuItem value={2} primaryText="IPA" />
-                           <MenuItem value={3} primaryText="Golden Ale" />
-                           <MenuItem value={4} primaryText="Agave Lager" />
+                    </TableBody>
+                </Table>
+                <FloatingActionButton
+                    onClick={this.handleOpenForm}
+                    backgroundColor={"grey"}
+                    style={{position:"fixed", right:25, bottom:25}}>
+                    <ContentAdd />
+                </FloatingActionButton>
+                <Dialog
+                    contentStyle={{width:'30%'}}
+                    open={this.state.openForm}
+                    onRequestClose={this.handleOpenForm}
+                >
+                    <form action="">
 
-                       </SelectField>
-                       <br/>
-                       <TextField
-                           floatingLabelFixed={true}
-                           floatingLabelText="Cantidad"
-                           hintText="Cantidad"/>
-                       <br/>
-                       <TextField
-                           floatingLabelFixed={true}
-                           floatingLabelText="Presentacion"
-                           hintText="Presentacion"/>
-                       <br/>
-                       <RaisedButton label={'Enviar Orden'}/>
+                        <SelectField
+                            fullWidth={true}
+                            floatingLabelText="Producto"
+                            value={1}
 
-                   </Paper>
-               </GridTile>
-               <GridTile cols={1} style={{padding:'3%'}}>
-                <Paper zDepth={2} style={{padding:'1%', marginBottom:'1%'}}>
-                    <p>Producto: Brow Ale</p>
-                    <p>Cantidad: 2 </p>
-                    <p>Presentación: Caja</p>
-                </Paper>
-                   <Paper zDepth={2} style={{padding:'1%' , marginBottom:'1%'}}>
-                       <p>Producto:  IPA</p>
-                       <p>Cantidad: 5 </p>
-                       <p>Presentación: Caja</p>
-                   </Paper>
-                   <Paper zDepth={2} style={{padding:'1%' , marginBottom:'1%'}}>
-                       <p>Producto: Agave</p>
-                       <p>Cantidad: 1 </p>
-                       <p>Presentación: SuperBo</p>
-                   </Paper>
-               </GridTile>
-           </GridList>
-        </div>
-    )
-};
+                        >
+                            <MenuItem value={1} primaryText="Brown Ale" />
+                            <MenuItem value={2} primaryText="IPA" />
+                            <MenuItem value={3} primaryText="Golden Ale" />
+                            <MenuItem value={4} primaryText="Agave Lager" />
 
-export default SolicitarProducto;
+                        </SelectField>
+                        <br/>
+                        <TextField
+                            fullWidth={true}
+                            floatingLabelFixed={true}
+                            floatingLabelText="Cantidad"
+                            hintText="Cantidad"/>
+                        <br/>
+                        <SelectField
+                            fullWidth={true}
+                            floatingLabelText="Presentacion"
+                            value={1}
+
+                        >
+                            <MenuItem value={1} primaryText="Caja" />
+                            <MenuItem value={2} primaryText="MiniPack" />
+                            <MenuItem value={3} primaryText="SpecialP Pack" />
+
+
+                        </SelectField>
+                        <RaisedButton label={'Enviar'} fullWidth={true}/>
+                    </form>
+                </Dialog>
+            </div>
+        );
+    }
+}
+
+//SolicitarProducto.propTypes = {
+// myProp: PropTypes.string.isRequired
+//};
+
+function mapStateToProps(state, ownProps) {
+    return {
+        state: state
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(dispatch)
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SolicitarProducto);
